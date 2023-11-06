463ff50d66bb5c14a73adfb027a7c48461aec66f
Riley Seaburg <riley@rileyseaburg.com>
2023-11-05 22:37:33 -0500

    Update create_service_account.sh

--- create_service_account.sh
+++ create_service_account.sh
@@ -9,12 +9,17 @@ CLUSTER_ROLE_BINDING_NAME="quantumforge-cluster-role-binding"
 # Create Namespace if it doesn't exist
 kubectl get namespace "$NAMESPACE" &> /dev/null || kubectl create namespace "$NAMESPACE"
 
-# Delete the existing ServiceAccount if it exists and recreate it
-kubectl delete serviceaccount "$SERVICE_ACCOUNT_NAME" --namespace "$NAMESPACE" --ignore-not-found
-kubectl create serviceaccount "$SERVICE_ACCOUNT_NAME" --namespace "$NAMESPACE"
+# Create or replace the ServiceAccount in the specified Namespace
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: $SERVICE_ACCOUNT_NAME
+  namespace: $NAMESPACE
+EOF
 
-# Create the ClusterRole
-cat <<EOF | kubectl apply -f -
+# Create or replace the ClusterRole
+kubectl apply -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
@@ -25,8 +30,8 @@ rules:
   verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
 EOF
 
-# Create the ClusterRoleBinding
-cat <<EOF | kubectl apply -f -
+# Create or replace the ClusterRoleBinding
+kubectl apply -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
@@ -41,12 +46,28 @@ subjects:
   namespace: $NAMESPACE
 EOF
 
-# Wait for the secret associated with the ServiceAccount to be created by the control plane
-sleep 5
+# Wait for the Secret associated with the ServiceAccount to be created
+echo "Waiting for ServiceAccount secret to be created..."
+for ((i=0; i<10; i++)); do
+  SERVICE_ACCOUNT_SECRET_NAME=$(kubectl get serviceaccount "$SERVICE_ACCOUNT_NAME" --namespace "$NAMESPACE" -o jsonpath='{.secrets[0].name}' --ignore-not-found)
+  if [ ! -z "$SERVICE_ACCOUNT_SECRET_NAME" ]; then
+    break
+  fi
+  sleep 1
+done
 
-# Get the ServiceAccount token
-SERVICE_ACCOUNT_SECRET_NAME=$(kubectl get serviceaccount "$SERVICE_ACCOUNT_NAME" --namespace "$NAMESPACE" -o jsonpath='{.secrets[0].name}')
+if [ -z "$SERVICE_ACCOUNT_SECRET_NAME" ]; then
+  echo "Failed to find the secret for ServiceAccount $SERVICE_ACCOUNT_NAME in namespace $NAMESPACE"
+  exit 1
+fi
+
+# Retrieve the ServiceAccount token
 TOKEN=$(kubectl get secret "$SERVICE_ACCOUNT_SECRET_NAME" --namespace "$NAMESPACE" -o jsonpath='{.data.token}' | base64 --decode)
+
+if [ -z "$TOKEN" ]; then
+  echo "Failed to retrieve the token for ServiceAccount $SERVICE_ACCOUNT_NAME"
+  exit 1
+fi
 
 # Output the token
 echo "Service Account Token: $TOKEN"
